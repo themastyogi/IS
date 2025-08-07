36543622f53772acb20c4e0f4c990f76ef1757cf
themastyogi <themastyogi@gmail.com>
2025-08-07 12:13:38 +0530

    Add files via upload

---
+++ app/page_tsx.ts
@@ -0,0 +1,19 @@
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Experience from '../components/Experience'
+import Skills from '../components/Skills'
+import Projects from '../components/Projects'
+import Contact from '../components/Contact'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Experience />
+      <Skills />
+      <Projects />
+      <Contact />
+    </>
+  )
+}
